1c9f912f1ec6c25720e8b3fae7f4a458f3d83e6e
larry fang <larry.l.fang@gmail.com>
2011-09-03 22:24:24 +1000

    test create operation of productController

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe ProductsController do
+   describe "index" do
+     it "lists all the products with image, title and price information"
 
+   end
+   describe "create" do
+     it "create a new product" do
+       product = mock_model(Product)
+       Product.should_receive(:new).and_return(product)
+       product.should_receive(:save)
+       post :create, :product=> {:title =>"jquery cookbook",
+                         :image_url => "http://www.abc.com",
+                         :description => "it's a book about how to do ruby'",
+                         :price => 12.3}
+     end
+
+   end
 end
