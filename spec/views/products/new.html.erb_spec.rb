1dd916889638b3b75e86e606930fd5bd63e426c6
larry fang <larry.l.fang@gmail.com>
2011-09-03 19:55:40 +1000

    one more example of spec

--- spec/views/products/new.html.erb_spec.rb
+++ spec/views/products/new.html.erb_spec.rb
@@ -1,21 +1,35 @@
 require "spec_helper"
 
 describe "products/new.html.erb" do
-
-  it "renders a form to create a new product" do
-    product = mock_model(Product,
+  let(:product) {mock_model(Product,
                          :title =>"jquery cookbook",
                          :image_url => "http://www.abc.com",
                          :description => "it's a book about how to do ruby'",
                          :price => 12.34)
-    assign(:product, product.as_new_record)
+                  }
+  before do
+    assign(:product, product.as_new_record.as_null_object)
+  end
+  it "renders a form to create a new product" do
+
     render
     rendered.should have_selector("form",
                                   :method=> "post",
                                   :action=> products_path
                     ) do |form|
       form.should have_selector("input", :type=>"submit")
-      form.should have_selector("input", :id=>"product_title", :name=>"product[title]")
+      form.should have_selector("input", :id=>"product_title",
+                                :name=>"product[title]")
+    end
+  end
+
+  it "renders a text field with price" do
+    product.stub(:price).and_return(12.3)
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("input",
+                              :name =>"product[price]",
+                              :value=> "12.3")
     end
   end
 end
