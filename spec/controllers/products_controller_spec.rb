eb7ff4ed27de841f5b277f6527b78334956927e9
larry fang <larry.l.fang@gmail.com>
2011-09-04 17:57:30 +1000

    get controller spec complete

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,20 +1,47 @@
 require 'spec_helper'
 
 describe ProductsController do
-   describe "index" do
-     it "lists all the products with image, title and price information"
+  describe "index" do
+    it "lists all the products with image, title and price information" do
+      product = mock_model(Product,
+                           :title =>"ruby cookbook",
+                           :image_url => "http://www.abc.com",
+                           :description => "it's a book about how to do ruby'",
+                           :price => "12.8")
+      products = []
+      3.times { products << product }
+      products.size.should == 3
+      Product.should_receive(:all).and_return(products)
+      post :index
+    end
+  end
+  describe "post create" do
+    context "when save is successful" do
+      it "create a new product" do
+        product = mock_model(Product)
+        Product.should_receive(:new).and_return(product)
+        product.should_receive(:save).and_return(true)
+        post :create, :product=> {:title =>"ruby cookbook",
+                                  :image_url => "http://www.abc.com",
+                                  :description => "it's a book about how to do ruby'",
+                                  :price => 12.8}
+        response.should redirect_to(product_path(assigns(:product)))
+      end
+    end
 
-   end
-   describe "create" do
-     it "create a new product" do
-       product = mock_model(Product)
-       Product.should_receive(:new).and_return(product)
-       product.should_receive(:save)
-       post :create, :product=> {:title =>"jquery cookbook",
-                         :image_url => "http://www.abc.com",
-                         :description => "it's a book about how to do ruby'",
-                         :price => 12.3}
-     end
+    context "when save is unsuccessful" do
+      it "create a new product" do
+        product = mock_model(Product)
+        Product.should_receive(:new).and_return(product)
+        product.should_receive(:save).and_return(false)
+        post :create, :product=> {:title =>"ruby cookbook",
+                                  :image_url => "http://www.abc.com",
+                                  :description => "it's a book about how to do ruby'",
+                                  :price => 12.8}
+        response.should render_template("new")
+      end
+    end
 
-   end
+
+  end
 end
