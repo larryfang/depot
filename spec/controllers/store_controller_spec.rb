1089dc3eae26f6e9f329da9f24c31c92649ab969
larry fang <larry.l.fang@gmail.com>
2011-09-05 11:42:07 +1000

    adding test case for store view

--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -4,6 +4,7 @@ describe StoreController do
 
   describe "GET 'index'" do
     it "should be successful" do
+      Product.should_receive(:order)
       get 'index'
       response.should be_success
     end
--- spec/views/store/index.html.erb_spec.rb
+++ spec/views/store/index.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe "store/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "display all products order by product title" do
+    products=[]
+    product = mock_model(Product,
+                         :title =>"ruby cookbook",
+                         :image_url => "http://www.abc.com",
+                         :description => "it's a book about how to do ruby'",
+                         :price => 12.6)
+    assign(:products, products<<product)
+    render
+    rendered.should contain(product.title)
+    rendered.should contain(product.description)
+    rendered.should contain(product.price.to_s)
+  end
 end
+
