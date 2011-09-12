99d80bb55c1abeb33f668617d71887ac3876b9f4
larry fang <larry.l.fang@gmail.com>
2011-09-12 20:45:38 +1000

    support smart cart calculation

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,9 @@
 module CartsHelper
+  def calculate
+   cart =  Cart.find(session[:cart_id])
+   cart.line_items.inject(0) do |total,item|
+     total + item.product.price
+   end
+
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ActiveRecord::Base
       message: 'must be a URL for GIF, JPG or PNG image.'
   }
 =end
+has_many :line_items
+  before_destroy :ensure_not_referenced_by_an_line_item
+
+  private
+  def ensure_not_referenced_by_an_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'line items present')
+      return false
+    end
+  end
 end
