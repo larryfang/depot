82ead9d4a2396366665d66bb3b273c0a7c507c42
larry fang <larry.l.fang@gmail.com>
2011-09-12 21:13:53 +1000

    include quantity

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -2,7 +2,7 @@ module CartsHelper
   def calculate
    cart =  Cart.find(session[:cart_id])
    cart.line_items.inject(0) do |total,item|
-     total + item.product.price
+     total + item.product.price * item.quantity
    end
 
   end
