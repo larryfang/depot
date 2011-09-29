22e362ffb887668743ef7c7443f83fc81cfdef8b
larry fang <larry.l.fang@gmail.com>
2011-09-29 21:15:29 +1000

    add a nice active record spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Cart do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have multiple line items created" do
+    cart = Cart.create
+    line_item1 =  LineItem.create(:product_id => 1, :cart => cart)
+    line_item2 =  LineItem.create(:product_id => 2, :cart => cart)
+    cart.line_items.find_by_product_id(1).should_not be_nil
+    cart.line_items.find_by_product_id(2).should_not be_nil
+
+  end
 end
